a2e3a7666ee83de8cc25341eeb7170121b5462d3
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-29 18:30:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,20 @@
 class Person
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+  
+  def name=(persons_name)
+    @name = persons_name
+  end 
+  def name
+    @name
+  end
+  
+  def job=(persons_job)
+    @job = persons_job
+  end
+  def job
+    @job
+  end
 end
